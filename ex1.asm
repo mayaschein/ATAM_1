600fd57fd141121a2514a1e22ab64339277caf75
mayaschein <maya.schein@campus.technion.ac.il>
2025-11-29 08:55:38 +0200

    some changes

--- ex1.asm
+++ ex1.asm
@@ -1,9 +1,5 @@
 .global _start
 
-.section .data
-vowels:
-	.byte 'a', 'e', 'i', 'o', 'u'
-	
 .section .text
 _start:
 	xor %esi, %esi # %esi=i is the counter for the string
@@ -11,14 +7,14 @@ _start:
 	movl %esi, count(%rip) # count=0
 	movb $'A', %cl
 	subb $'a', %cl
-loop:
-	movb string(%rip, %esi, 1), %bl # copy the i-th letter to %bl
+loop_hw1:
+	movb string(, %esi, 1), %bl # copy the i-th letter to %bl
 	cmp 0, %bl # check whether we are at the end of the string
 	je end
 check_vowels:
-	cmp %bl, vowels(%rip, %eax, 1)
+	cmp %bl, vowels(, %eax, 1)
 	je found
-	movb vowels(%rip, %eax, 1), %bh 
+	movb vowels(, %eax, 1), %bh 
 	addb %cl, %bh # %bh=vowel+('A'-'a')
 	cmp %bl, %bh
 	je found
@@ -35,4 +31,5 @@ found:
 	inc %esi
 	jmp loop
 end:
-nop
+
+
